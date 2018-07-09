9357a6a1b8c6631518db465ccfea371e09e63bab
quinnHuang <quinn.huang@oocl.com>
2018-07-09 15:49:48 +0800

    [Quinn] complete practice 3

--- src/practice_3/student.js
+++ src/practice_3/student.js
@@ -1,1 +1,26 @@
+// 写一个Person类，要有name，age属性，要有一个introduce方法，
+// introduce方法返回一个字符串形如：
+//
+// >My name is Tom. I am 21 years old.
+//
+// 再写一个Student类继承Person类，除了name，age属性，还有要有class属性。也有一个introduce方法，
+// introduce方法返回一个字符串形如：
+//
+// >I am a Student. I am at Class 2.
+//
+//
+// 再写一个Worker类继承Person类，只有name，age属性。也有一个introduce方法，
+// introduce方法返回一个字符串形如：
+//
+// >I am a Worker. I have a job.
 
+import Person from "./person.js";
+export default class Student extends Person{
+  constructor(name,age,klass){
+    super(name,age);
+    this.klass=klass;
+  }
+  introduce(){
+    return "I am a Student. I am at Class "+this.klass+".";
+  }
+}
--- src/practice_3/worker.js
+++ src/practice_3/worker.js
@@ -1,1 +1,23 @@
+// 写一个Person类，要有name，age属性，要有一个introduce方法，
+// introduce方法返回一个字符串形如：
+//
+// >My name is Tom. I am 21 years old.
+//
+// 再写一个Student类继承Person类，除了name，age属性，还有要有class属性。也有一个introduce方法，
+// introduce方法返回一个字符串形如：
+//
+// >I am a Student. I am at Class 2.
+//
+//
+// 再写一个Worker类继承Person类，只有name，age属性。也有一个introduce方法，
+// introduce方法返回一个字符串形如：
+//
+// >I am a Worker. I have a job.
 
+import Student from "./student.js";
+import Person from "./person.js";
+export default class Worker extends Person {
+  introduce(){
+    return "I am a Worker. I have a job.";
+  }
+}
