c1f1bb7d27d434d1ce92bce4abe7593385c4a07a
quinnHuang <quinn.huang@oocl.com>
2018-07-09 16:52:25 +0800

    [Quinn] complete practice 7

--- src/practice_7/teacher-option2.js
+++ src/practice_7/teacher-option2.js
@@ -1,1 +1,24 @@
+import Person from "./person.js";
+import Class from "./class.js";
+import Student from "./student.js";
 
+export default class Teacher extends Person {
+  constructor(name,age,klass) {
+    super(name,age);
+    this.klass=klass;
+  }
+  introduce(){
+    if (this.klass) {
+      return super.introduce()+" I am a Teacher. I teach "+this.klass.getDisplayName()+".";
+    }else{
+      return super.introduce()+" I am a Teacher. I teach No Class.";
+    }
+  }
+  introduceWith(student){
+    if (this.klass===student.klass) {
+      return super.introduce()+" I am a Teacher. I teach "+student.name+".";
+    }else {
+      return super.introduce()+" I am a Teacher. I don't teach "+student.name+".";
+    }
+  }
+}
